1d4651edf00aab4bf53bf0430ad0e03a7ac95a36
Joseph Schorr <jschorr@gmail.com>
2016-02-24 20:55:48 -0500

    Allow generics on nominal types

---
+++ generator/es5/tests/nominal/generic.js
@@ -0,0 +1,128 @@
+$module('generic', function () {
+  var $static = this;
+  this.$class('SomeClass', false, function () {
+    var $static = this;
+    var $instance = this.prototype;
+    $static.new = function () {
+      var instance = new $static();
+      var init = [];
+      return $promise.all(init).then(function () {
+        return instance;
+      });
+    };
+    $instance.DoSomething = function () {
+      var $this = this;
+      var $state = $t.sm(function ($callback) {
+        while (true) {
+          switch ($state.current) {
+            case 0:
+              $state.resolve($t.nominalwrap(true, $g.____testlib.basictypes.Boolean));
+              return;
+
+            default:
+              $state.current = -1;
+              return;
+          }
+        }
+      });
+      return $promise.build($state);
+    };
+  });
+
+  this.$type('MyType', true, function (T) {
+    var $instance = this.prototype;
+    var $static = this;
+    this.new = function ($wrapped) {
+      var instance = new this();
+      instance.$wrapped = $wrapped;
+      return instance;
+    };
+    $instance.AnotherThing = function () {
+      var $this = this;
+      var $state = $t.sm(function ($callback) {
+        while (true) {
+          switch ($state.current) {
+            case 0:
+              $t.nominalunwrap($this).DoSomething().then(function ($result0) {
+                $result = $result0;
+                $state.current = 1;
+                $callback($state);
+              }).catch(function (err) {
+                $state.reject(err);
+              });
+              return;
+
+            case 1:
+              $state.resolve($result);
+              return;
+
+            default:
+              $state.current = -1;
+              return;
+          }
+        }
+      });
+      return $promise.build($state);
+    };
+    $instance.SomeProp = $t.property(function () {
+      var $this = this;
+      var $state = $t.sm(function ($callback) {
+        while (true) {
+          switch ($state.current) {
+            case 0:
+              $state.resolve($t.nominalwrap(true, $g.____testlib.basictypes.Boolean));
+              return;
+
+            default:
+              $state.current = -1;
+              return;
+          }
+        }
+      });
+      return $promise.build($state);
+    });
+  });
+
+  $static.TEST = function () {
+    var m;
+    var sc;
+    var $state = $t.sm(function ($callback) {
+      while (true) {
+        switch ($state.current) {
+          case 0:
+            $g.generic.SomeClass.new().then(function ($result0) {
+              $result = $result0;
+              $state.current = 1;
+              $callback($state);
+            }).catch(function (err) {
+              $state.reject(err);
+            });
+            return;
+
+          case 1:
+            sc = $result;
+            m = $t.nominalwrap(sc, $g.generic.MyType($g.____testlib.basictypes.Integer));
+            m.SomeProp().then(function ($result0) {
+              return m.AnotherThing().then(function ($result1) {
+                $result = $t.nominalwrap($t.nominalunwrap($result0) && $t.nominalunwrap($result1), $g.____testlib.basictypes.Boolean);
+                $state.current = 2;
+                $callback($state);
+              });
+            }).catch(function (err) {
+              $state.reject(err);
+            });
+            return;
+
+          case 2:
+            $state.resolve($result);
+            return;
+
+          default:
+            $state.current = -1;
+            return;
+        }
+      }
+    });
+    return $promise.build($state);
+  };
+});
