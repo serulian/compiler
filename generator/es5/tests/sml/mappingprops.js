a64020626dd7ad083e11c3afbebd7d29f3f6dbb6
Joseph Schorr <jschorr@gmail.com>
2016-07-05 22:35:51 -0400

    Add optimization to use direct object literals if all keys are literals

--- generator/es5/tests/sml/mappingprops.js
+++ generator/es5/tests/sml/mappingprops.js
@@ -39,12 +39,9 @@ $module('mappingprops', function () {
       while (true) {
         switch ($current) {
           case 0:
-            $g.____testlib.basictypes.Mapping($g.____testlib.basictypes.String).overObject(function () {
-              var obj = {
-              };
-              obj["SomeProp"] = $t.box("hello world", $g.____testlib.basictypes.String);
-              return obj;
-            }()).then(function ($result1) {
+            $g.____testlib.basictypes.Mapping($g.____testlib.basictypes.String).overObject({
+              SomeProp: $t.box("hello world", $g.____testlib.basictypes.String),
+            }).then(function ($result1) {
               return $g.mappingprops.SimpleFunction($result1).then(function ($result0) {
                 $result = $result0;
                 $current = 1;
