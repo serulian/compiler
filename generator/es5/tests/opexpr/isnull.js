e0e2e7060b9f2ae86c152c5bf36168bde932ea2e
Joseph Schorr <jschorr@gmail.com>
2016-08-05 21:41:28 -0400

    Update generation of `is` operator for `not` sub operator

--- generator/es5/tests/opexpr/isnull.js
+++ generator/es5/tests/opexpr/isnull.js
@@ -1,10 +1,33 @@
 $module('isnull', function () {
   var $static = this;
   $static.DoSomething = function (a) {
+    var b;
     var $current = 0;
     var $continue = function ($resolve, $reject) {
-      $resolve($t.box(a == null, $g.____testlib.basictypes.Boolean));
-      return;
+      while (true) {
+        switch ($current) {
+          case 0:
+            b = $t.box(1234, $g.____testlib.basictypes.Integer);
+            $promise.resolve(a == null).then(function ($result0) {
+              $result = $t.box($result0 && (b != null), $g.____testlib.basictypes.Boolean);
+              $current = 1;
+              $continue($resolve, $reject);
+              return;
+            }).catch(function (err) {
+              $reject(err);
+              return;
+            });
+            return;
+
+          case 1:
+            $resolve($result);
+            return;
+
+          default:
+            $resolve();
+            return;
+        }
+      }
     };
     return $promise.new($continue);
   };
