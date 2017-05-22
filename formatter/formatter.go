56475d9f16a034446e1cdb028f82502022f5e0d5
Joseph Schorr <jschorr@gmail.com>
2017-05-21 21:22:19 -0400

    Add ability in formatter to format source code in a string

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -17,7 +17,7 @@ import (
 	"github.com/serulian/compiler/compilerutil"
 	"github.com/serulian/compiler/parser"
 
-	"github.com/ryanuber/go-glob"
+	glob "github.com/ryanuber/go-glob"
 )
 
 type importHandlingOption int
@@ -103,6 +103,11 @@ func Format(path string, debug bool) bool {
 	return formatFiles(path, importHandlingInfo{importHandlingNone, []string{}, []string{}, false}, debug)
 }
 
+// FormatSource formats the given Serulian source code.
+func FormatSource(source string) (string, error) {
+	return formatSource(source, importHandlingInfo{importHandlingNone, []string{}, []string{}, false})
+}
+
 // formatFiles runs formatting of all matching source files found at the given source path.
 func formatFiles(path string, importHandling importHandlingInfo, debug bool) bool {
 	if !debug {
@@ -127,35 +132,33 @@ func parseAndFormatSourceFile(sourceFilePath string, info os.FileInfo, importHan
 		return err
 	}
 
-	// Conduct the parsing.
-	parseTree := newParseTree(source)
-	inputSource := compilercommon.InputSource(sourceFilePath)
-	rootNode := parser.Parse(parseTree.createAstNode, nil, inputSource, string(source))
-
-	// Report any errors found.
-	if len(parseTree.errors) > 0 {
-		for _, err := range parseTree.errors {
-			startRune, _ := strconv.Atoi(err.properties[parser.NodePredicateStartRune])
-			sal := compilercommon.NewSourceAndLocation(inputSource, startRune)
-			location := sal.Location()
-
-			fmt.Printf("%v: line %v, column %v: %s\n",
-				sourceFilePath,
-				location.LineNumber()+1,
-				location.ColumnPosition()+1,
-				err.properties[parser.NodePredicateErrorMessage])
-		}
-
-		return fmt.Errorf("Parsing errors found in file %s", sourceFilePath)
+	formatted, err := formatSource(string(source), importHandling)
+	if err != nil {
+		return err
 	}
 
-	// Create the formatted source.
-	formattedSource := buildFormattedSource(parseTree, rootNode.(formatterNode), importHandling)
-	if string(formattedSource) == string(source) {
+	if string(formatted) == string(source) {
 		// Nothing changed.
 		return nil
 	}
 
 	// Overwrite the file with the formatted source.
-	return ioutil.WriteFile(sourceFilePath, formattedSource, info.Mode())
+	return ioutil.WriteFile(sourceFilePath, []byte(formatted), info.Mode())
+}
+
+// formatSource formats the given Serulian source code, with the given import handling.
+func formatSource(source string, importHandling importHandlingInfo) (string, error) {
+	// Conduct the parsing.
+	parseTree := newParseTree([]byte(source))
+	inputSource := compilercommon.InputSource("(formatting)")
+	rootNode := parser.Parse(parseTree.createAstNode, nil, inputSource, string(source))
+
+	// Report any errors found.
+	if len(parseTree.errors) > 0 {
+		return "", fmt.Errorf("Parsing errors found in source")
+	}
+
+	// Create the formatted source.
+	formattedSource := buildFormattedSource(parseTree, rootNode.(formatterNode), importHandling)
+	return string(formattedSource), nil
 }
