2bdafeec6f2187163fc3023229bd7f36d38ff36f
Joseph Schorr <jschorr@gmail.com>
2017-06-04 23:14:24 -0400

    Add semver upgrade helper to compilerutil
    Both formatting and Grok will make use of this new method

---
+++ compilerutil/semver.go
@@ -0,0 +1,54 @@
+// Copyright 2015 The Serulian Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package compilerutil
+
+import (
+	"github.com/blang/semver"
+)
+
+// UpdateVersionOption defines the options for the SemanticUpdateVersion method.
+type UpdateVersionOption string
+
+const (
+	// UpdateVersionMinor indicates that only minor version updates are allowed.
+	UpdateVersionMinor UpdateVersionOption = "minor"
+
+	// UpdateVersionMajor indicates the both minor and major version updates are allowed.
+	UpdateVersionMajor = "major"
+)
+
+// SemanticUpdateVersion finds the semver in the available versions list that is newer than the current
+// version.
+func SemanticUpdateVersion(currentVersionString string, availableVersions []string, option UpdateVersionOption) (string, error) {
+	currentVersion, err := semver.ParseTolerant(currentVersionString)
+	if err != nil {
+		return "", err
+	}
+
+	latestVersionString := currentVersionString
+	for _, possibleVersion := range availableVersions {
+		// Skip empty version strings.
+		if len(possibleVersion) == 0 {
+			continue
+		}
+
+		// Skip possibleVersions that don't parse, as well as pre-release versions
+		// (since they are, by definition, not considered stable).
+		parsed, err := semver.ParseTolerant(possibleVersion)
+		if err != nil || len(parsed.Pre) > 0 {
+			continue
+		}
+
+		// Find the latest stable version.
+		if parsed.GT(currentVersion) {
+			if option == UpdateVersionMajor || parsed.Major == currentVersion.Major {
+				currentVersion = parsed
+				latestVersionString = possibleVersion
+			}
+		}
+	}
+
+	return latestVersionString, nil
+}
